07490cd48c0fdcc7d4e81632cdd85e81887557cb
lukew2251 <59976033+lukew2251@users.noreply.github.com>
2021-02-24 13:43:32 -0600

    Add files via upload

---
+++ Project1LW.py
@@ -0,0 +1,21 @@
+#import os, getfile,zipfile
+
+listofnames = ('ATTACK-RESPONSES',
+ 'BAD-TRAFFIC', 'CHAT IRC', 'COMMUNITY WEB',
+ 'DNS SPOOF', 'ET ACTIVEX', 'ET CHAT IRC',
+ 'ET CNC', 'ET CURRENT_EVENTS', 'ET DNS',
+ 'ET EXPLOIT', 'ET INFO', 'ET MALWARE', 'ET P2P',
+ 'ET POLICY', 'ET SCAN', 'ET SHELLCODE', 'ET TROJAN',
+ 'ET USER_AGENTS', 'ET WEB_CLIENT', 'ET WEB_SERVER',
+ 'ET WEB_SPECIFIC_APPS', 'GPL', 'ICMP', 'INFO',
+ 'MS-SQL', 'MULTIMEDIA', 'P2P', 'POLICY', 'SCAN',
+ 'WEB-ATTACKS', 'WEB-CGI', 'WEB-CLIENT', 'WEB-FRONTPAGE',
+ 'WEB-IIS', 'WEB-MISC', 'WEB-PHP')
+#Def parse
+#Def maj_des
+#Def classif
+#Def clean_up
+with open('alert.full.maccdc2012.csv','a') in my_file
+    my_name = "Luke Willis"
+    split1 = my_name.split()
+    
