4f4e334bc3ca004c96956301e413cf8ff37e0b30
lukew2251 <59976033+lukew2251@users.noreply.github.com>
2021-04-05 19:01:10 -0500

    Add files via upload

--- Mod09Homework.ps1
+++ Mod09Homework.ps1
@@ -14,10 +14,39 @@ $cost = $base_price + $rand_price
 Write-Host "Welcome to the soda machine. You can enter values of 5, 10, or 25 cents in payment."
 $choice = Read-Host "What type of soda would you like" 
 Write-Host ("The current price of $choice is $cost cents")
+if ($choice -ne "")
+{
+    Write-Host ("The current price of $choice is $cost cents")
+    break
+}
+else
+{
+    Write-Host "You must enter a value. Press Enter to try again."
+    Read-Host
+    clear
+    continue
+}
 
 while ($true)
 {
     $input = Read-Host "Enter a coin: "
+    if ($input -eq 5)
+    {
+        Write-Host "You have inserted a nickel."
+    }
+    elseif ($input -eq 10)
+    {
+        Write-Host "You have inserted a dime."
+    }
+    elseif ($input -eq 25)
+    {
+        Write-Host "You have inserted a quarter."
+    }
+    else
+    {
+        Write-Host "That is not a valid amount."
+        continue
+    }
     $pay += $input
     $balance = $cost - $pay
     if ($balance -gt 0)
@@ -35,3 +64,4 @@ while ($true)
         Write-Host ("Enjoy your $choice !")
     }
 }
+Write-Host "Press enter to close the program: "
