f5dc58281ac4f8a3a1ef013af9228e09dfbcbb8e
lukew2251 <59976033+lukew2251@users.noreply.github.com>
2021-04-26 11:12:51 -0500

    Add files via upload

---
+++ Mod12Tutorial.ps1
@@ -0,0 +1,107 @@
+﻿#Luke Willis
+#Mod 12
+
+$employee_string="Rudy T. Navarro,586 New St.,Anchorage,Jennie K. Carlson,468 West Old Drive,Charlotte,Kristine Z. Blackwell,363 Yellow Duck Way,Fresno,Ivan N. Marquez,876 Rocky Second Street,Yonkers,Clarence H. Baird,915 North Nobel St.,San Antonio,Tia J. Li,780 Rocky Cherrywood Street,Indianapolis,Tammy O. Holder,882 West Crabapple Avenue,Toledo,Nicole K. Perkins,58 Old St.,El Paso,Spencer M. Cross,48 North Narwhal St.,Atlanta,Robin T. Fernandez,51 West Rocky Lakeridge St.,Grand Rapids,Cindy J. Oconnor,925 Green New Parkway,Atlanta,Ian K. Sosa,117 Oak Way,Toledo,Betsy O. Rowland,60 Nobel Road,Omaha,Jeremiah E. Sexton,247 North First Street,Las Vegas,Yvette U. Gomez,159 Nobel Drive,Grand Rapids,Marisol F. Henderson,988 Rocky Oak Avenue,Mobile,Deborah C. Mayer,86 South Yellow New Street,Columbus,Wendell H. Coffey,85 South Rocky Crabapple Blvd.,Atlanta,Robin W. Frederick,310 Oak St.,Jackson,Chrystal I. Bowers,726 Second Road,San Antonio,Norma L. Sexton,86 Cherrywood Drive,Cleveland,Lea R. Kirby,87 New Parkway,Seattle,Jessica M. Cline,31 Green Fabien Road,Anchorage,Maggie P. Cortez,348 East Oak Road,Jackson,Levi Z. Riddle,979 Rocky Fabien Road,Tacoma,Melanie I. Trevino,304 Hague Avenue,Virginia Beach,Loren O. Pineda,97 Milton St.,Philadelphia,Jesse H. Adkins,15 Cherrywood Way,Bakersfield,Garry A. Allen,309 Green Elm Street,Hialeah,Victoria R. Olson,397 South Hague Avenue,Seattle,Roland E. Buchanan,828 Green New Parkway,Fort Worth,Austin C. Roman,423 New Boulevard,Columbus,Debra B. Mullins,164 South Rocky Second Drive,Atlanta,Perry G. Gonzales,443 New Way,Greensboro,Tracey Z. Collier,87 Rocky Narwhal Avenue,Dallas,Janice X. Waller,122 Nobel St.,Louisville,Lillian N. Sullivan,20 Green Narwhal Avenue,Tacoma,Glenda E. Herrera,757 Cowley Blvd.,Tampa,Courtney B. Leblanc,316 North Cherrywood Road,Kansas,Connie C. Spencer,52 Crabapple Way,Los Angeles,Ricky P. Nichols,117 Duck Avenue,Miami,Lamar G. Aguilar,246 Rocky Lakeridge Boulevard,Spokane,Elisabeth S. Moran,608 Second Avenue,Albuquerque,Ethan C. Juarez,53 Green Fabien Avenue,Hialeah,Marcus X. Underwood,20 North Duck Road,Colorado,Gina C. Davies,203 First Road,Fremont,Allan D. Scott,95 South Oak Parkway,Glendale,Cara S. Delgado,841 West First Way,Arlington,Patrick Q. Watts,403 South Rocky Lakeridge Boulevard,Buffalo,Roger F. Lawrence,344 Rocky Fabien Freeway,Montgomery,Bryan M. Bruce,206 South Oak Boulevard,New York,Samantha E. Hansen,511 North Lakeridge Freeway,Portland,Tanya D. Riley,593 Green Crabapple Freeway,San Antonio,Erika F. Ramsey,70 West Green Duck Parkway,Sacramento,Colby F. Page,695 Duck Way,Mesa,Theodore G. Burton,290 First Street,Omaha,Lorie W. Finley,35 South Rocky Nobel Freeway,Virginia Beach,Traci V. Charles,69 Yellow Lakeridge Parkway,Memphis,Beth P. Hanna,91 East Rocky Cowley Street,Fort Worth,Tricia W. Payne,12 Yellow Oak Avenue,Mesa,Laurie N. Fields,729 Green Cowley Drive,Aurora,Tommy U. Hudson,196 Cherrywood Avenue,Stockton,Shawn P. Parrish,579 First Street,Jackson,Kent F. Harrington,46 Lakeridge Freeway,Des Moines,Garry G. Mcintyre,79 Yellow First Avenue,Toledo,Janette D. Gordon,65 East Rocky Old Drive,Baton Rouge,Christian T. Hanna,208 Oak Way,St. Petersburg,Kerri N. Hampton,791 Rocky Milton Way,Buffalo,Bradley L. Powell,607 Duck Street,Newark,Dion M. Rangel,544 Yellow Fabien Parkway,El Paso,Shauna Y. Tyler,30 Yellow Hague Boulevard,Washington,Shirley J. Kelly,917 Rocky Old Blvd.,Fort Worth,Preston L. Zuniga,337 East Cherrywood Drive,Memphis,Oliver D. Hodges,608 Rocky First Road,San Diego,Marcella F. Hays,54 Rocky Crabapple St.,Shreveport,Rick E. Herrera,109 Rocky Narwhal Boulevard,San Jose,Loretta A. Parrish,36 West Nobel Street,St. Petersburg,Scotty U. Crawford,773 Rocky Fabien Road,El Paso,Jeff J. Sandoval,258 West Green Cowley St.,Fresno,Bryan M. French,86 Nobel Boulevard,San Diego,Felicia Y. Rasmussen,214 South Green Hague Boulevard,Fort Worth,Kate X. Valdez,454 Cowley Boulevard,Oakland,Tommy F. Reese,48 Second Way,Nashville,Leon Y. Warren,884 Oak Parkway,Spokane,Raul J. Jenkins,85 Yellow Narwhal Blvd.,Albuquerque,Darius T. Luna,876 First Freeway,Tampa,Tonia X. Gilmore,146 Cherrywood Road,Washington,Katina R. Brady,923 Rocky Elm Drive,Norfolk,Timothy R. Snyder,412 Old St.,Los Angeles,Jesse P. Ochoa,33 Yellow Fabien Parkway,Des Moines,Ronnie O. Cochran,508 East Yellow Hague Way,Hialeah,Shad P. Krueger,721 Old Street,Omaha,Dean Y. Bruce,905 Narwhal Road,Denver,Bobbie C. Cochran,96 Yellow Cherrywood Way,Garland,Gustavo L. Cohen,88 Yellow Second Way,Charlotte,Alberto A. Shepard,25 North Hague Blvd.,Riverside,Janelle E. Larson,968 Yellow Old Boulevard,Dallas,Veronica F. Wagner,130 Hague Blvd.,Tampa,Anita U. Padilla,82 Hague Blvd.,Mobile,Lorraine Z. Clements,573 Green Crabapple Way,Glendale,Tasha F. Pearson,97 Green Duck Freeway,Spokane,Lawrence G. Cordova,972 Elm Boulevard,Dayton,Blanca J. Oneal,122 Yellow Milton Way,Jacksonville,Brandie K. Smith,987 Fabien Way,Tacoma,Dan G. Ortiz,236 East Rocky New Drive,Philadelphia,Marisa K. Walters,896 Green Oak St.,Cincinnati,Jimmie D. Green,76 First Avenue,Tampa,Kareem N. Ritter,754 West Fabien Boulevard,Yonkers,Tyrone R. Castillo,450 Rocky Second St.,Sacramento,Lori W. Carroll,62 Rocky Oak Freeway,Madison,Maggie M. French,24 Clarendon Freeway,Oklahoma,Bethany G. Oliver,24 Rocky Milton Parkway,Chicago,Roberta Q. Santana,762 East Clarendon Avenue,Louisville,Nicholas Q. Conley,997 Nobel Freeway,St. Petersburg,Anita U. Mack,647 Yellow Milton Blvd.,Fresno,Larry B. Sampson,87 Green Narwhal Avenue,Little Rock,Ian A. Klein,266 New Street,Columbus,Aisha H. Contreras,46 Yellow Narwhal Drive,Denver,Kari D. Schaefer,895 Green Cowley Way,Long Beach,Gabrielle K. Archer,44 North Nobel Freeway,Norfolk,Andrew T. Bird,20 North Oak Freeway,Phoenix,Cara V. Atkinson,553 West Narwhal Boulevard,Mobile,Susan M. Mcdaniel,530 Lakeridge Street,Fresno,Ralph O. Hatfield,20 First Parkway,Pittsburgh,Cody E. Woodard,515 Oak Boulevard,Memphis,Lewis P. Huynh,542 Narwhal Drive,Long Beach,Wendell R. Carey,739 Nobel Parkway,Tucson,Dennis U. Morris,86 North Cherrywood Freeway,Little Rock,William J. Dean,25 South Rocky Crabapple Blvd.,Dayton,Anitra N. Carson,702 First St.,Sacramento,Anthony V. Bean,42 South Hague Avenue,Portland,Aimee Y. Cummings,90 First Parkway,Las Vegas,Joan N. Cisneros,16 Green Hague Drive,Tampa,Josh Z. Murillo,535 Milton Freeway,Garland,Vincent Z. Adkins,94 Yellow New Parkway,Fremont,Colby X. Barron,44 East Rocky Lakeridge Drive,Albuquerque,Deborah I. Lynch,806 Nobel Freeway,Los Angeles,Kyle A. Holden,280 Yellow Nobel Boulevard,Mesa,Rickey F. Duncan,676 Rocky New Boulevard,Riverside,Marlon T. Brennan,28 West First Blvd.,Los Angeles,Cari E. Fleming,31 East Second Street,Tacoma,Michele Y. Gonzalez,73 Second Road,Buffalo,Adrienne X. Mora,52 East Hague Blvd.,St. Petersburg,Derek R. Baird,47 Yellow Duck Road,Miami,Lakesha K. Rojas,47 Yellow Clarendon St.,Miami,Alana G. Bishop,862 East Hague Road,Miami,Cassie I. Mejia,18 Green Nobel Blvd.,Des Moines,Nikki D. Petersen,13 New Parkway,Akron,Keisha T. Spears,78 Second Way,Cincinnati,Johnathan J. Suarez,659 Lakeridge Freeway,Lincoln"
+
+$employee_array = @()
+
+#task 1
+Write-Host "Task 1`r`n"
+
+#make path to the user desktop
+$user_path = "C:\Users\" + $env:USERNAME + "\Desktop"
+Set-Location $user_path
+
+#turn the big string into a handy array
+$employee_array = $employee_string.Split(",")
+
+#write the header
+Write-Output ("Name,Address,City") | Out-File "employee_data.csv" -Encoding ascii
+
+#write the data
+for ($i=0; $i -lt $employee_array.Count; $i = $i + 3)
+{
+    Write-Output ($employee_array[$i] + "," +
+    $employee_array[$i+1] + "," + 
+    $employee_array[$i+2]) | Out-File "employee_data.csv" -Encoding ascii -Append
+}
+
+#task 2
+Write-Host "Task 2`r`n"
+
+$file = ".\employee_data.csv"
+$employee_by_city = @()
+
+#make array of city arrays
+foreach ($line in Get-Content $file)
+{
+    if ($line.ReadCount -eq 1)
+    {
+        continue
+    }
+    $split1 = $line.split(",")
+    $city = $split1[2].trim(" ")
+    $employee_by_city += ,@($city + ": ")
+}
+
+#unique sort the city array
+$employee_by_city = $employee_by_city | sort -Unique
+
+#add last names to city arrays
+foreach ($line in Get-Content $file)
+{
+    if ($line.ReadCount -eq 1)
+    {
+        continue
+    }
+    $split1 = $line.Split(",")
+    $full_name = $split1[0]
+    $last_name = $full_name.Split(" ")[2]
+    $last_name = $last_name.Trim(" ")
+    $city = $split1[2].Trim(" ")
+    for ($i=0; $i -lt $employee_by_city.Count; $i++)
+    {
+        if ($city + ": " -eq $employee_by_city[$i][0])
+        {
+            $employee_by_city[$i] += $last_name
+        }
+    }
+}
+
+#write out the array elements
+foreach ($i in $employee_by_city)
+{
+    Write-Host $i
+}
+
+#task 3
+Write-Host "Task 3`r`n"
+
+#check for archives subdirectory off desktop, make if not there
+if (-Not (Test-Path .\archives_Willis))
+{
+    mkdir .\archives_Willis | Out-Null
+}
+
+#make a zip file of the csv in the archives subdirectory
+$zipfile = @{
+    Path = "employee_data.csv"
+    CompressionLevel = "Fastest"
+    DestinationPath = ".\archives_Willis\empdata01"
+}
+Compress-Archive @zipfile -Force
+
+#remove the csv file from the desktop
+del .\employee_data.csv
+
+#check for the extracted files subdirectory
+if (-Not (Test-Path .\extracted_files_Willis))
+{
+    mkdir .\extracted_files_Willis | Out-Null
+}
+
+#unzip the csv file into the the extracted_file subdirectory
+Expand-Archive -Path .\archives_Willis\empdata01.zip -DestinationPath .\extracted_files_Willis -Force
+
+#pause
+Read-Host "`r`nPress Enter to exit the script"
